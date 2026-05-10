457d16482d5aea381835f4fe7332313fbf0fdada
Gabriel Duarte <gabrielduarte933@proton.me>
2026-05-10 20:11:51 -0300

    Updated Query 4

--- queries/q4.sql
+++ queries/q4.sql
@@ -3,7 +3,7 @@
     da esfera federal.
 */
 
-SELECT M.nome_municipio AS Municipio
+SELECT M.nome_municipio AS Municipio, O.nome_orgao AS Orgao
 FROM Municipio AS M
 INNER JOIN Orgao AS O ON M.codigo_ibge = O.codigo_municipio
 WHERE O.esfera = 'federal';
