74141c0f8217fab1ec3a68a207309a3e2afdea24
Gabriel Duarte <gabrielduarte933@proton.me>
2026-06-08 12:43:55 -0300

    Updated Query 8.

--- queries/q08_total_gasto_por_grupo.sql
+++ queries/q08_total_gasto_por_grupo.sql
@@ -1,15 +1,35 @@
 -- total gasto por grupo
 
-WITH ClasseGrupo AS (
-    SELECT C.codigo_classe, G.codigo_grupo, G.nome_grupo, G.tipo_item
-    FROM Classe as C
+WITH GastosPorGrupo AS (
+    SELECT 
+        G.codigo_grupo, 
+        G.nome_grupo, 
+        G.tipo_item,
+        SUM(I.valor_estimado) AS subtotal
+    FROM ItemLicitacao AS I
+    INNER JOIN Material AS M ON I.codigo_item_material = M.codigo_item
+    INNER JOIN Classe AS C ON M.codigo_classe = C.codigo_classe
     INNER JOIN Grupo AS G ON C.codigo_grupo = G.codigo_grupo
+    GROUP BY G.codigo_grupo, G.nome_grupo, G.tipo_item
+
+    UNION ALL
+
+    SELECT 
+        G.codigo_grupo, 
+        G.nome_grupo, 
+        G.tipo_item,
+        SUM(I.valor_estimado) AS subtotal
+    FROM ItemLicitacao I
+    INNER JOIN Servico S ON I.codigo_item_servico = S.codigo_servico
+    INNER JOIN Classe C ON S.codigo_classe = C.codigo_classe
+    INNER JOIN Grupo G ON C.codigo_grupo = G.codigo_grupo
+    GROUP BY G.codigo_grupo, G.nome_grupo, G.tipo_item
 )
-SELECT CG.codigo_grupo, CG.nome_grupo, COALESCE(SUM(I.valor_estimado), 0) as total, CG.tipo_item
-FROM ItemLicitacao AS I
-LEFT JOIN Material AS M ON I.codigo_item_material = M.codigo_item
-LEFT JOIN Servico AS S ON I.codigo_item_servico = S.codigo_servico
-INNER JOIN ClasseGrupo CG ON (I.codigo_item_material IS NOT NULL AND M.codigo_classe = CG.codigo_classe) OR
-                                 (I.codigo_item_servico IS NOT NULL AND S.codigo_classe = CG.codigo_classe)
-GROUP BY CG.codigo_grupo, CG.nome_grupo, CG.tipo_item
+SELECT 
+    codigo_grupo, 
+    nome_grupo, 
+    COALESCE(SUM(subtotal), 0) AS total, 
+    tipo_item
+FROM GastosPorGrupo
+GROUP BY codigo_grupo, nome_grupo, tipo_item
 ORDER BY total DESC;
