9f53d48568b9f6cbc4891f676db6af2873d6edc4
Gabriel Duarte <gabrielduarte933@proton.me>
2026-06-08 09:08:45 -0300

    Added query 8 'Total gasto por grupo'.

---
+++ queries/q8_total_gasto_por_grupo.sql
@@ -0,0 +1,32 @@
+-- total gasto por grupo
+
+WITH ClasseGrupo AS (
+    SELECT C.codigo_classe, G.codigo_grupo, G.nome_grupo
+    FROM Classe as C
+    INNER JOIN Grupo AS G ON C.codigo_grupo = G.codigo_grupo
+),
+MaterialGrupo AS (
+    SELECT M.codigo_item, CG.codigo_grupo, CG.nome_grupo
+    FROM Material AS M
+    INNER JOIN ClasseGrupo AS CG ON M.codigo_classe = CG.codigo_classe
+),
+ServicoGrupo AS (
+    SELECT S.codigo_servico, CG.codigo_grupo, CG.nome_grupo
+    FROM Servico AS S
+    INNER JOIN ClasseGrupo AS CG ON S.codigo_classe = CG.codigo_classe 
+),
+TotalGastoMaterial AS (
+    SELECT codigo_grupo, nome_grupo, SUM(COALESCE(valor_estimado, 0)) AS total_gasto
+    FROM ItemLicitacao AS IL
+    INNER JOIN MaterialGrupo AS MG ON IL.codigo_item_material = MG.codigo_item
+    GROUP BY codigo_grupo, nome_grupo
+),
+TotalGastoServico AS (
+    SELECT codigo_grupo, nome_grupo, SUM(valor_estimado) AS total_gasto
+    FROM ItemLicitacao AS IL
+    INNER JOIN ServicoGrupo AS SG ON IL.codigo_item_servico = SG.codigo_servico
+    GROUP BY codigo_grupo, nome_grupo
+)
+SELECT * FROM TotalGastoMaterial
+UNION SELECT * FROM TotalGastoServico
+ORDER BY total_gasto DESC;
