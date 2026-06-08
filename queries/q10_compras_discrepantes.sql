c0cdecb915649d56ea3f40ad7f019f0bddf0b1d0
Gabriel Duarte <gabrielduarte933@proton.me>
2026-06-08 09:55:09 -0300

    Added query 10 'Compras discrepantes'.

---
+++ queries/q10_compras_discrepantes.sql
@@ -0,0 +1,28 @@
+-- Seleciona as compras que foram realizadas com preço
+-- acima do praticado no mercado
+
+WITH PrecoMercado AS (
+    SELECT codigo_item_material as codigo_material,
+           codigo_item_servico as codigo_servico,
+          AVG(valor_estimado / NULLIF(quantidade, 0)) AS preco_medio
+    FROM ItemLicitacao
+    WHERE situacao_item = 'HOMOLOGADO' AND quantidade > 0
+    GROUP BY codigo_item_material, codigo_item_servico
+    HAVING AVG(valor_estimado / NULLIF(quantidade, 0)) > 0
+)
+SELECT I.id_compra,
+       I.id_compra_item,
+       COALESCE(M.nome_material, S.nome_servico) as nome_item,
+       (I.valor_estimado / I.quantidade) AS preco_unitario_praticado,
+       ROUND(PM.preco_medio, 2) AS preco_medio_mercado,
+       ROUND((I.valor_estimado / I.quantidade) / PM.preco_medio, 2) AS vezes_acima
+FROM ItemLicitacao AS I
+LEFT JOIN Material AS M ON I.codigo_item_material = M.codigo_item
+LEFT JOIN Servico AS S ON I.codigo_item_servico = S.codigo_servico
+JOIN PrecoMercado AS PM ON 
+    (I.codigo_item_material = PM.codigo_material OR 
+     I.codigo_item_servico = PM.codigo_servico)
+WHERE I.situacao_item = 'HOMOLOGADO' AND quantidade > 0 AND 
+      (I.valor_estimado / I.quantidade) > (PM.preco_medio * 3)
+ORDER BY vezes_acima DESC
+LIMIT 15;
