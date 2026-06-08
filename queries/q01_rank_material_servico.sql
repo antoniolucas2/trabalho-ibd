97a711455baa56abd1dca03b45172fb768368dd2
Gabriel Duarte <gabrielduarte933@proton.me>
2026-06-08 10:17:19 -0300

    Added query 1 'Rank materiais/servicos mais comprados'.

---
+++ queries/q01_rank_material_servico.sql
@@ -0,0 +1,20 @@
+/**
+    Top 15 Materiais ou Servicos mais comprados
+*/
+
+SELECT COALESCE(M.nome_material, S.nome_servico) as nome_item,
+        CASE 
+            WHEN i.codigo_item_material IS NOT NULL THEN 'Material'
+            ELSE 'Servico'
+        END AS tipo, 
+    COUNT(I.id_compra_item) AS vezes_comprado,
+    SUM(I.quantidade) AS volume_total_itens, 
+    SUM(I.valor_estimado) AS valor_total_estimado
+FROM ItemLicitacao AS I
+LEFT JOIN Material AS M ON I.codigo_item_material = M.codigo_item
+LEFT JOIN Servico AS S ON I.codigo_item_servico = S.codigo_servico
+WHERE I.situacao_item = 'HOMOLOGADO'
+GROUP BY nome_item, I.codigo_item_material, I.codigo_item_servico
+ORDER BY vezes_comprado DESC
+LIMIT 15;
+    
