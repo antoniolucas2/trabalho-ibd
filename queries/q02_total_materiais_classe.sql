607382e0aa59c9330869280e96bcf4a8640b1ec6
Gabriel Duarte <gabrielduarte933@proton.me>
2026-06-13 14:00:37 -0300

    Updated Query 02

--- queries/q02_total_materiais_classe.sql
+++ queries/q02_total_materiais_classe.sql
@@ -2,18 +2,18 @@
     Total gasto com materiais por classe.    
 */
 
-WITH AllMateriais AS (
+WITH TodosMateriais AS (
     SELECT IL.id_compra, IL.codigo_item_material, 
            IL.valor_estimado as valor_total
     FROM ItemLicitacao AS IL
 ),
-AllMateriaisClass AS (
+TodosMateriaisClasses AS (
     SELECT AM.*, C.nome_classe, C.codigo_classe 
     FROM Material AS M
-    INNER JOIN AllMateriais AS AM ON M.codigo_item = AM.codigo_item_material
+    INNER JOIN TodosMateriais AS TM ON M.codigo_item = TM.codigo_item_material
     INNER JOIN Classe AS C ON C.codigo_classe = M.codigo_classe
 )
-SELECT AMC.nome_classe, AMC.codigo_classe, COALESCE(SUM(AMC.valor_total), 0) AS valor_gasto
-FROM AllMateriaisClass AS AMC
-GROUP BY AMC.nome_classe, AMC.codigo_classe
+SELECT TMC.nome_classe, TMC.codigo_classe, COALESCE(SUM(TMC.valor_total), 0) AS valor_gasto
+FROM TodosMateriaisClasses AS TMC
+GROUP BY TMC.nome_classe, TMC.codigo_classe
 ORDER BY valor_gasto DESC;
